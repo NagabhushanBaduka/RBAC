e8c22b1b3296c1536ecde03191fb292c10638cae
Baduka Naga Bhushan <nagabhushanbadka@gmail.com>
2024-12-06 10:59:05 +0530

    Initial commit: Added frontend and backend folders

---
+++ Backend/models/permissions.js
@@ -0,0 +1,10 @@
+class Permissions{
+    constructor(){
+        this.permissions=[];
+    }
+    getPermissionsByRoleName(roleName){
+        const role=roles.roles.find((r)=>r.name===roleName);
+        return role ? role.permissions:[];
+    }
+}
+module.exports=Permissions;
